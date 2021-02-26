dbf3ff33e0f04606150761bda129514222e4bb15
Edson Almeida <79293954+EdsonAlmeidaa@users.noreply.github.com>
2021-02-26 18:54:50 -0300

    Melhoria na estrutura do código

--- python/Jogo_adivinhacao.py
+++ python/Jogo_adivinhacao.py
@@ -4,18 +4,24 @@ print('*************************************')
 print('Bem-vindo(a) no jogo de adivinhação! ')
 print('*************************************')
 
-
 numero_secreto = 42 #Variavél
-print(type(numero_secreto)) #Informando o tipo da variável.
 
 chute_str = input('Digite o seu número: ') #solicitando ao usuário 
+
 print('Você digitou ', chute_str) # Visualizando o que foi digitado pelo usuário.
 
 chute = int(chute_str) # Variável
 
-if numero_secreto == chute: 
-    print('Você acertou"')
+acertou = chute == numero_secreto
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
+if (acertou): 
+    print('Parabéns! Você acertou"')
 else:
-    print('Você errou!')
+    if(maior):
+        print('Você errou! O seu chute foi maior do que o número secreto.')
+    elif(menor):
+        print('Você errou! O seu chute foi menor do que o número secreto.')
 
 print('Fim!')
