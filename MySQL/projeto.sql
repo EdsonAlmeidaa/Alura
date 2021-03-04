641d6ba9e7c33db6c8095d91d8e0573db189aa22
Edson Almeida <79293954+EdsonAlmeidaa@users.noreply.github.com>
2021-03-04 18:47:41 -0300

    Inserção de Dados
    Inserção de dados na tabela vendedores

--- MySQL/projeto.sql
+++ MySQL/projeto.sql
@@ -63,3 +63,12 @@ insert into tbVendedores (
 values (
     '00233', 'João Geraldo da Fonseca', 0.10);
 
+insert into tbVendedores (
+    MATRICULA, NOME, PERCENTUAL_COMISSAO)
+values (
+    '00235', 'Márcio Almeida Silva', 0.08);
+
+insert into tbVendedores (
+    MATRICULA, NOME, PERCENTUAL_COMISSAO)
+values (
+    '00236', 'Cláudia Morais', 0.08);
