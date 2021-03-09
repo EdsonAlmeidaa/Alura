f718c8c1ca34c01ec506727c58f9005d1890cdca
Edson Almeida <79293954+EdsonAlmeidaa@users.noreply.github.com>
2021-03-09 19:26:52 -0300

    Add Nível do Jogo
    Código para adicionar nível no jogo.

--- python/Jogo_adivinhacao.py
+++ python/Jogo_adivinhacao.py
@@ -10,6 +10,18 @@ numero_secreto = random.randrange(1, 101) #Variavél
 total_de_tentativas = 3
 rodada = 1
 
+print('Qual nível de dificuldade?')
+print('(1) Fácil (2) Médio (3) Difícil')
+
+nivel = int(input('Define o nível: '))
+
+if(nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
+    
 for rodada in range(1, total_de_tentativas + 1):
     print('Tentativa {} de {}'.format(rodada, total_de_tentativas))
     chute_str = input('Digite o seu número: ') #solicitando ao usuário 
