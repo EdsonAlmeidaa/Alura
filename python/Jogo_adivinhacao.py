375ef20f10bad444b3380362b23fd90251ff433f
Edson Almeida <79293954+EdsonAlmeidaa@users.noreply.github.com>
2021-03-09 19:39:35 -0300

    Add de pontuação do usuário
    Código para demostrar a pontuação do usuário no jogo.

--- python/Jogo_adivinhacao.py
+++ python/Jogo_adivinhacao.py
@@ -9,6 +9,7 @@ print('*************************************')
 numero_secreto = random.randrange(1, 101) #Variavél
 total_de_tentativas = 3
 rodada = 1
+pontos = 1000
 
 print('Qual nível de dificuldade?')
 print('(1) Fácil (2) Médio (3) Difícil')
@@ -21,10 +22,10 @@ elif(nivel == 2):
     total_de_tentativas = 10
 else:
     total_de_tentativas = 5
-    
+
 for rodada in range(1, total_de_tentativas + 1):
     print('Tentativa {} de {}'.format(rodada, total_de_tentativas))
-    chute_str = input('Digite o seu número: ') #solicitando ao usuário 
+    chute_str = input('Digite um número entre 1 e 100: ') #solicitando ao usuário 
     print('Você digitou ', chute_str) # Visualizando o que foi digitado pelo usuário.
     chute = int(chute_str) # Variável
 
@@ -37,12 +38,14 @@ for rodada in range(1, total_de_tentativas + 1):
     menor = chute < numero_secreto
 
     if (acertou): 
-        print('Parabéns! Você acertou"')
+        print('Você acertou e fez {} pontos!'.format(pontos))
         break
     else:
         if(maior):
             print('Você errou! O seu chute foi maior do que o número secreto.')
         elif(menor):
             print('Você errou! O seu chute foi menor do que o número secreto.')
-
+        pontos_perdidos = abs(numero_secreto - chute)     
+        pontos = pontos - pontos_perdidos
+   
 print('Fim do jogo!')
