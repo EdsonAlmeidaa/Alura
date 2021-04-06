171b4100dd067343d5469c527cad03e983ecdcf8
Edson Almeida <79293954+EdsonAlmeidaa@users.noreply.github.com>
2021-04-06 11:39:26 -0300

    Continuação do jogo forca
    Usei  a função .strip() para remover espaços em branco à direita e à esquerda da letra digitada.
    
    Usei  a função .upper() para evitarmos diferenças entre maiúsculas e minúsculas. Assim, comparando tanto o chute quanto a letra iterada em maiúscula.

--- python/forca.py
+++ python/forca.py
@@ -11,13 +11,14 @@ def jogar():
     while(not enforcou and not acertou):
 
         chute = input('Qual letra? ')
+        chute = chute.strip() # Eliminar os erros por causa dos espaços em branco.
 
         index = 0
         for letra in palavra_secreta:
-            if (chute == letra):
+            if (chute.upper() == letra.upper()): # Eliminar os erros de letras MAIÚSCULAS
                 print('Encontrei a letra {} na posição {}.'.format(letra, index))
             index = index + 1
-            
+
         print('jogando....')
 
     print('Fim do jogo')
