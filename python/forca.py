5a4c422e6c9532cadf6e39f513a20088f230cca7
Edson Almeida <79293954+EdsonAlmeidaa@users.noreply.github.com>
2021-04-06 11:08:36 -0300

    Continuação do Loop While

--- python/forca.py
+++ python/forca.py
@@ -9,8 +9,17 @@ def jogar():
     acertou = False
 
     while(not enforcou and not acertou):
+
+        chute = input('Qual letra? ')
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute == letra):
+                print('Encontrei a letra {} na posição {}.'.format(letra, index))
+            index = index + 1
+            
         print('jogando....')
-        
+
     print('Fim do jogo')
 
 if __name__ == '__main__':
