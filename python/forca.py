1e1987e8ef7a2c7a151759e09e9bd04c5b901102
Edson Almeida <79293954+EdsonAlmeidaa@users.noreply.github.com>
2021-04-06 16:40:50 -0300

    Implementando o encerramento do jogo FORCAR

--- python/forca.py
+++ python/forca.py
@@ -3,27 +3,43 @@ def jogar():
     print('***Bem vindo no jogo de Forca!***')
     print('*********************************')
 
-    palavra_secreta = 'banana'
-    letras_acertadas = ['_', '_', '_', '_', '_', '_']
+    palavra_secreta = 'banana'.apper()
+    letras_acertadas = ['_' for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
     while(not enforcou and not acertou):
 
         chute = input('Qual letra? ')
-        chute = chute.strip() # Eliminar os erros por causa dos espaços em branco.
+        chute = chute.strip().upper() # Eliminar os erros por causa dos espaços em branco. Adicionei o (upper)para retornar tudo maisculo
 
-        index = 0
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()): # Eliminar os erros de letras MAIÚSCULAS
-                letras_acertadas[index] = letra
-            index = index + 1
+        if chute in palavra_secreta:
+            index = 0
+            
+            for letra in palavra_secreta:
+                
+                if chute== letra: # Eliminar os erros de letras MAIÚSCULAS
+                    letras_acertadas[index] = letra
+              
+                index += 1
 
+        else:
+            erros += 1
+            print("Ops, você errou! Faltam {} tentativas.".format(6-erros))
+
+        enforcou = erros == 6
+        acertou = '_' not in letras_acertadas
         print(letras_acertadas)
 
+    if acertou:
+        print('Você Ganhou!')
+    else:
+        print('Você Perdeu!')
+    
     print('Fim do jogo')
 
 if __name__ == '__main__':
