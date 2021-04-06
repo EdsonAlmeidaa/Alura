921397e183982adcc709c5b248605a1803bf33bd
Edson Almeida <79293954+EdsonAlmeidaa@users.noreply.github.com>
2021-04-06 20:59:34 -0300

    Criando Função
    Funçao (imprime_mensagem_abertura():)
    Funçao (carrega_palavra_secreta())
    Funçao (inicializa__letras_acertadas)

--- python/forca.py
+++ python/forca.py
@@ -1,23 +1,14 @@
 import random
+
+
+
+
 def jogar():
-    print('*********************************')
-    print('***Bem vindo no jogo de Forca!***')
-    print('*********************************')
+    imprime_mensagem_abertura()
+    palavra_secreta = carrega_palavra_secreta()
 
-    #Leitura do arquivo:
-    arquivo = open('palavra.txt', 'r')
-    palavra = []
-
-    for linha in arquivo:
-        linha = linha.strip()
-        palavra.append(linha)
-
-    arquivo.close()
-
-    numero = random.randrange(0, len(palavra))
-    palavra_secreta = palavra.upper()
-    
-    letras_acertadas = ['_' for letra in palavra_secreta]
+    letras_acertadas = inicializa__letras_acertadas(palavra_secreta)
+    print(letras_acertadas)
 
     enforcou = False
     acertou = False
@@ -54,6 +45,29 @@ def jogar():
         print('Você Perdeu!')
     
     print('Fim do jogo')
+
+def inicializa__letras_acertadas(palavra):
+    return ['_' for letra in palavra]
+
+def imprime_mensagem_abertura():
+    print('*********************************')
+    print('***Bem vindo no jogo de Forca!***')
+    print('*********************************')
+
+def carrega_palavra_secreta():
+    #Leitura do arquivo:
+    arquivo = open('palavra.txt', 'r')
+    palavra = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavra.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavra))
+    palavra_secreta = palavra[numero].upper()
+    return palavra_secreta
 
 if __name__ == '__main__':
     jogar()
